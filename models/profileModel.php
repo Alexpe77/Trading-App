a46edfc174d0bdb068102bcb0b8a044ca04637f7
Alexpe77 <126241961+Alexpe77@users.noreply.github.com>
2023-08-23 12:08:41 +0200

    Add updateProfile function

--- models/profileModel.php
+++ models/profileModel.php
@@ -17,6 +17,15 @@ class ProfileModel {
         $statement->execute();
 
         return $statement->fetchAll(\PDO::FETCH_ASSOC);
+    }
 
+    public function updateProfile() {
+        $query = "UPDATE profile SET first_name = :first_name, last_name = :last_name, address = :address
+        WHERE id = :id";
+        $statement = $this->db->prepare($query);
+        $statement->bindParam(':id', PDO::PARAM_INT);
+        $statement->bindParam(':first_name', PDO::PARAM_STR);
+        $statement->bindParam(':last_name', PDO::PARAM_STR);
+        $statement->bindParam(':address', PDO::PARAM_STR);
     }
 }
